4543a591b3372df479fd9c94a9f494bbe5ff835c
Saidivya2001 <94470189+Saidivya2001@users.noreply.github.com>
2022-03-09 13:30:06 +0530

    uploaded html and js files of "calculator"

---
+++ js.js
@@ -0,0 +1,9 @@
+function userinput(value){
+    document.getElementById("userinput").value;
+}
+function a(){
+    var p=document.getElementById("userinput").value;
+    var q=eval(p);
+    document.getElementById("userinput").value=q;
+}
+
